ab7445501f89538194d342da689ac5310e87bbec
irfanfy <irfanfy@CATR4>
2022-12-26 18:39:37 +0400

    Add project files.

---
+++ Lab retry/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab_retry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ikireqemli eded daxil edin");
+            string input = Console.ReadLine();
+            if (input.Length < 2 || input.Length > 2)
+            {
+                Console.WriteLine("Eded ikireqemli deyil");
+                return;
+            }
+            int number = int.Parse(input);
+            int lastDigit = number % 10;
+            number /= 10;
+            Console.WriteLine($"{number} {lastDigit}");
+        
+                
+        }
+    }
+}
